5881dfadc5e64e071a8e392a621a8e922617d95a
amrmagdy744 <amrmagdy744@gmail.com>
2024-05-04 14:03:01 +0300

    amr XD

--- firebase/auth.js
+++ firebase/auth.js
@@ -11,7 +11,7 @@ import {
 } from "@firebase/auth";
 import { db } from "./Config";
 import { collection } from "@firebase/firestore";
-import {setDoc, doc, getDoc} from "@firebase/firestore"
+import {setDoc, doc, getDoc, updateDoc} from "@firebase/firestore"
 // Listen for authentication state to change.
 onAuthStateChanged(auth, (user) => {
   if (user != null) {
@@ -20,8 +20,12 @@ onAuthStateChanged(auth, (user) => {
 
   // Do other things
 });
-
+async function updateuser(user){
+  const currentUserid = auth.currentUser.uid;
+  
+}
 async function register(email, password, name, phone, code) {
+  
   const cred = await createUserWithEmailAndPassword(auth, email, password);
   const usercol = collection(db, "users");
   const docRef = doc(usercol, cred.user.uid);
